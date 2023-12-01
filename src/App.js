6e058f328c8b7cb8cd3a17e7081f242a8c131748
DineshReacts <dinesh635803@gmail.com>
2023-12-01 19:51:02 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import Header from "./Component/Header";
+import Content from "./Component/Content";
+function App(){
+  return(
+    <div>
+      <Header />
+      <Content />
+    </div>
+  );
+}
+export default App;
